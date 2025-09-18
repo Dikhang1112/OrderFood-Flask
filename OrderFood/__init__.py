ae151e3cb65566a0f5669d599c1b3910c736fa3e
HoangVanDung <vandunghoang2004@gmail.com>
2025-09-18 18:46:12 +0700

    deploy

--- OrderFood/__init__.py
+++ OrderFood/__init__.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-please-change-me")
 
 SQLALCHEMY_DATABASE_URI = os.getenv(
     "SQLALCHEMY_DATABASE_URI",
-    "mysql+pymysql://root:%s@localhost/orderfooddb?charset=utf8mb4" % quote("Admin@123"),
+    "mysql+pymysql://orderfood:%s@172.31.46.106/orderfooddb?charset=utf8mb4" % quote("Admin@123"),
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", "false").lower() == "true"
 
